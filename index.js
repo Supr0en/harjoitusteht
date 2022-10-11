5b19c8ab5684b0e4843173cf800c5005fd4c464c
Erik Ruotsalainen <94750392+Supr0en@users.noreply.github.com>
2022-10-11 07:55:44 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 import getPlayerScores from "./playerScores.js";
 
 export default async function Sorter() {
-  try {
-    const playerScores = await getPlayerScores();
-    let players = [];
-    let count = [];
-    for (let i = 0; i < playerScores.length; i++){
-      const pelaaja1 = Object.values(playerScores[i])[0];
-      const pelaaja2 = Object.values(playerScores[i])[1];
-      if (pelaaja1 > pelaaja2){
-        players.push(Object.keys(playerScores[i])[0]);
-      } else if(pelaaja1 < pelaaja2){
-        players.push(Object.keys(playerScores[i])[1]);
+   try {
+      const playerScores = await getPlayerScores();
+      let players = [];
+      let count = [];
+      for (let i = 0; i < playerScores.length; i++) {
+         const player1 = Object.entries(playerScores[i])[0];
+         const player2 = Object.entries(playerScores[i])[1];
+         for (let name of Object.keys(playerScores[i])) {
+            if (!players.includes(name)) {
+               players.push({
+                  name: name,
+                  wins: 0,
+               });
+               if (player1[1] > player2[1]) {
+                  players[i].wins += 1;
+               } else if (player1[1] < player2[1]) {
+                  players[i].wins += 1;
+               }
+            }
+         }
+        //  if (player1[1] > player2[1]) {
+        //     players[i].wins += 1;
+        //  } else if (player1[1] < player2[1]) {
+        //     players[i].wins += 1;
+        //  }
       }
-    }
-    players.forEach(item => (count[item] ? count[item]++ : count[item] = 1));
-    console.log(playerScores);
-    console.log(count);
-  } catch (error) {
-    console.error(`ERROR: ${error}`);
-  }
+      // players.forEach(item => (count[item] ? count[item]++ : count[item] = 1));
+      console.log(playerScores);
+      console.log(players);
+   } catch (error) {
+      console.error(`ERROR: ${error}`);
+   }
 }
 Sorter();
