ae0d448f585a212801d787479f4ca2737f692b99
Supr0en <Supr0en>
2022-10-12 07:38:59 +0300

    dublicatujen objectejen korjaus

--- index.js
+++ index.js
@@ -4,30 +4,23 @@ export default async function Sorter() {
    try {
       const playerScores = await getPlayerScores();
       let players = [];
-      let count = [];
       for (let i = 0; i < playerScores.length; i++) {
          const player1 = Object.entries(playerScores[i])[0];
          const player2 = Object.entries(playerScores[i])[1];
          for (let name of Object.keys(playerScores[i])) {
-            if (!players.includes(name)) {
+            if (!players.some((el) => el.name === name)) {
                players.push({
                   name: name,
                   wins: 0,
                });
-               if (player1[1] > player2[1]) {
-                  players[i].wins += 1;
-               } else if (player1[1] < player2[1]) {
-                  players[i].wins += 1;
-               }
             }
          }
-        //  if (player1[1] > player2[1]) {
-        //     players[i].wins += 1;
-        //  } else if (player1[1] < player2[1]) {
-        //     players[i].wins += 1;
-        //  }
+         for (let j = 0; j < players.length; j++) {
+            if (player1[1] > player2[1] || player1[1] < player2[1]) {
+               players[j].wins++;
+            }
+         }
       }
-      // players.forEach(item => (count[item] ? count[item]++ : count[item] = 1));
       console.log(playerScores);
       console.log(players);
    } catch (error) {
